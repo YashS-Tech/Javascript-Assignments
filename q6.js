7c8ad508fadb5d68e64992a29fac8a1efbda0de5
YashS-Tech <yash.satkar@techprescient.com>
2023-02-16 12:14:34 +0530

    Program 6 added

---
+++ q6.js
@@ -0,0 +1,15 @@
+//Q6:. Write a program to print keys and values of the given object.
+
+const input= {
+    a: 1,
+    b: 2
+    
+  };
+  
+  console.log(Object.keys(input));
+  console.log(Object.values(input));
+
+  /*OUTPUT
+  [ 'a', 'b' ]
+[ 1, 2 ]
+*/
