ebbed969b41543fced9ba114f1af61ae1d56dbf3
YashS-Tech <yash.satkar@techprescient.com>
2023-02-16 12:13:12 +0530

    Program 5 added

---
+++ q5.js
@@ -0,0 +1,13 @@
+//Write a program to join all elements of the array into a string with a given seperator.
+
+
+myColor = ["Red", "Green", "White", "Black"];
+console.log(myColor.toString());
+console.log(myColor.join());
+console.log(myColor.join('+'));
+
+/*OUTPUT:
+Red,Green,White,Black
+Red,Green,White,Black
+Red+Green+White+Black
+*/
