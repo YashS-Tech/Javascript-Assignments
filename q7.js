71f2a9a1925780194ff081a1bff4c2b539519b56
YashS-Tech <yash.satkar@techprescient.com>
2023-02-17 18:15:53 +0530

    Program 7 changed

--- q7.js
+++ q7.js
@@ -1,24 +1,31 @@
 //Q7.Write a program to find the most frequent item of an array
-var arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
-var mf = 1;
-var m = 0;
-var item;
-for (var i=0; i<arr1.length; i++)
-{
-        for (var j=i; j<arr1.length; j++)
-        {
-                if (arr1[i] == arr1[j])
-                 m++;
-                if (mf<m)
-                {
-                  mf=m; 
-                  item = arr1[i];
-                }
-        }
-        m=0;
-}
-console.log(item+" ( " +mf +" times ) ") ;
 
-/*OUTPUT:
-a ( 5 times )
-*/
+function mostFrequentItem(array) {
+    if (array.length === 0) {
+      return null; // handle empty array
+    }
+  
+    let maxCount = 0;
+    let maxItem = null;
+  
+    for (let i = 0; i < array.length; i++) {
+      let count = 1;
+  
+      for (let j = i + 1; j < array.length; j++) {
+        if (array[j] === array[i]) {
+          count++;
+        }
+      }
+  
+      if (count > maxCount) {
+        maxCount = count;
+        maxItem = array[i];
+      }
+    }
+  
+    return maxItem;
+  }
+
+  let array = [1, 2, 3, 3, 3, 4, 4, 5, 5];
+console.log(mostFrequentItem(array));
+
