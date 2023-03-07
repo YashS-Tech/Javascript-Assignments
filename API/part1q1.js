d98ea90401d5bc31d9672a58f9ecf83c71900286
YashS-Tech <yash.satkar@techprescient.com>
2023-03-07 13:10:54 +0530

    Program 2 part 1

--- API/part1q1.js
+++ API/part1q1.js
@@ -1,11 +1,1 @@
-const axios = require('axios');
-const url = 'https://api.github.com/gists/public';
 
-axios.get(url)
-  .then(response => {
-    const data = response.data.slice(0, 5);
-    console.log(data);
-  })
-  .catch(error => {
-    console.error(error);
-  });
