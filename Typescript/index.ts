ab9ba5cd850fe4c5436d692132290ce45f12df30
YashS-Tech <yash.satkar@techprescient.com>
2023-04-07 16:28:04 +0530

    added seperate model file

--- Typescript/index.ts
+++ Typescript/index.ts
@@ -1,125 +1,13 @@
 //import express from 'express';
 import express, { Request, Response } from "express";
 import bodyParser from 'body-parser';
-import { Sequelize, DataTypes, Model } from 'sequelize';
 
+import { User, UserDetail} from './model';
 // Initialize Express app
 const app = express();
 //app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Define Sequelize connection
-const sequelize = new Sequelize({
-  dialect: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: 'root123',
-  database: 'yashdb',
-});
-
-// Define User model
-class User extends Model {
-    
-        public id!: number;
-        public name!: string;
-        public username!: string;
-        public email!: string;
-        public readonly createdAt!: Date;
-        public readonly updatedAt!: Date;
-      
-}
-User.init({
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  email: {
-    type: DataTypes.STRING,
-    unique:true,
-    allowNull: false,
-    validate: {
-        isEmail: {
-        // args:true
-          msg: 'Invalid email format',
-        },
-        async customValidateEmail(value: string) {
-          // Custom validation to check email domain
-          if (!value.endsWith('@gmail.com')) {
-            throw new Error('Email domain must be @gmail.com');
-          }
-        },
-      },
-    },
-   
-  /*isAdmin: {
-    type: DataTypes.BOOLEAN,
-    defaultValue: false,
-  },*/
-}, {
-  sequelize,
-  modelName: 'User',
-  tableName: 'user',
-  timestamps: false,
-});
-
-// Define UserDetail model
-class UserDetail extends Model  {
-    public id!: number;
-    public addressStreet!: string;
-    public addressSuite!: string;
-    public addressCity!: string;
-    public addressZipcode!: string;
-    public phone!: string;
-    public readonly createdAt!: Date;
-    public readonly updatedAt!: Date;
-  }
-UserDetail.init({
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  address_street: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  address_suite: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  address_city: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  address_zipcode: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  phone: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-}, {
-  sequelize,
-  modelName: 'UserDetail',
-  tableName: 'user_details',
-  timestamps: false,
-});
-
-// Define User-UserDetail association
-User.hasOne(UserDetail, { foreignKey: 'id' });
-UserDetail.belongsTo(User, { foreignKey: 'id' });
 
 
 app.get('/users', async (req: Request, res: Response) => {
@@ -135,6 +23,11 @@ app.post('/users', async (req, res) => {
     /*
     if (!/^\S+@\S+\.\S+$/.test(email)) {
         return res.status(400).json({ message: 'Invalid email format' });*/
+
+        const emailRegex = /\S+@\S+\.\S+/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ message: 'Invalid email format' });
+    }
 
     // Create new User and UserDetail records
     const user = await User.create({ name, username, email, /*isAdmin: true*/ });
